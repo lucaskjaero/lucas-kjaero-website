fb5700d13b6f7075635d141f67e47723b16f608e
Lucas Kjaero <lucas@lucaskjaero.com>
2019-12-08 21:25:40 -0800

    Copy pitch from old site

--- gatsby-starter-hero-blog/src/components/Hero/Hero.js
+++ gatsby-starter-hero-blog/src/components/Hero/Hero.js
@@ -10,8 +10,14 @@ const Hero = props => {
     <React.Fragment>
       <section className="hero">
         <h1>
-          This is a demo site of&nbsp;the <strong>heroBlog</strong> GatsbyJS starter
+          The perfect addition to your team
         </h1>
+        <h2>
+        I'm a committed data scientist with a strong technical background and a broadly focused undergraduate education. I will use technically sound data analysis with an eye to what makes the data important.<br />
+        I was a fellow at Carnegie Mellon analyzing traffic data, and have worked on projects in diverse areas such as stock trading algorithms and Mandarin Chinese. Our team's stock trading algorithm made $10,000 (virtual) in the first week of testing.
+        I'm passionate about bringing valuable and actionable insights from your data. I work best with teams.
+        I look forward to working with you.
+        </h2>
         <button onClick={scrollToContent} aria-label="scroll">
           <FaArrowDown />
         </button>
