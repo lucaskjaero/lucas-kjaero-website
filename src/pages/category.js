07716c2456f5e0f155bedc0af5e8fc9fa4ac32f9
Lucas Kjaero <lucas@lucaskjaero.com>
2019-12-09 23:07:15 -0800

    Remove the rest of the cover

--- src/pages/category.js
+++ src/pages/category.js
@@ -97,15 +97,6 @@ export const query = graphql`
             title
             category
             author
-            cover {
-              children {
-                ... on ImageSharp {
-                  fluid(maxWidth: 800, maxHeight: 360) {
-                    ...GatsbyImageSharpFluid_withWebp
-                  }
-                }
-              }
-            }
           }
         }
       }
