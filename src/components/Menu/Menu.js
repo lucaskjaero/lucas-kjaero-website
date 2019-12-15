062e3c3081b57f7f2ca99b0b30e81ce8cb63a2c6
Lucas Kjaero <lucas@lucaskjaero.com>
2019-12-15 14:19:50 -0800

    Link resume

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,10 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 require("core-js/fn/array/from");
 
-import { FaHome } from "react-icons/fa/";
-import { FaEnvelope } from "react-icons/fa/";
-import { FaTag } from "react-icons/fa/";
-
+import { FaEnvelope, FaHome, FaTag, FaUserTie } from "react-icons/fa/";
 import Item from "./Item";
 import Expand from "./Expand";
 
@@ -24,6 +21,7 @@ class Menu extends React.Component {
     this.items = [
       { to: "/", label: "Home", icon: FaHome },
       { to: "/category/", label: "Categories", icon: FaTag },
+      { to: "/lucas-kjaero-zhang-resume.pdf", label: "Resume", icon: FaUserTie },
       ...pages,
       { to: "/contact/", label: "Contact", icon: FaEnvelope }
     ];
