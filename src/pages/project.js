02a12f9b4dede07010eaf800054ca15f3d9ba2e7
Lucas Kjaero <lucas@lucaskjaero.com>
2019-12-17 08:04:25 -0800

    Make page stateful

--- src/pages/project.js
+++ src/pages/project.js
@@ -9,73 +9,79 @@ import List from "../components/List";
 import Seo from "../components/Seo";
 import TechnologySelector from "../components/TechnologySelector";
 
-const ProjectPage = props => {
-  const {
-    data: {
-      posts: {
-        edges: posts,
-        technologies: technologies
-      }
-    }
-  } = props;
-
-  // Create category list
-  const categories = {};
-  posts.forEach(edge => {
-    const {
-      node: {
-        frontmatter: { category }
-      }
-    } = edge;
-
-    if (category && category != null) {
-      if (!categories[category]) {
-        categories[category] = [];
-      }
-      categories[category].push(edge);
-    }
-  });
-
-  const categoryList = [];
-
-  for (var key in categories) {
-    categoryList.push([key, categories[key]]);
+class ProjectPage extends React.Component {
+  constructor(props) {
+    super(props);
   }
 
-  return (
-    <React.Fragment>
-      <ThemeContext.Consumer>
-        {theme => (
-          <Article theme={theme}>
-            <header>
-              <Headline title="Projects by category" theme={theme} />
-            </header>
-            <TechnologySelector technologies={technologies} onChange={change => console.log(change)} />
-            {categoryList.map(item => (
-              <section key={item[0]}>
-                <h2>
-                  <FaTag /> {item[0]}
-                </h2>
-                <List edges={item[1]} theme={theme} />
-              </section>
-            ))}
-            {/* --- STYLES --- */}
-            <style jsx>{`
-              h2 {
-                margin: 0 0 0.5em;
-              }
-              h2 :global(svg) {
-                height: 0.8em;
-                fill: ${theme.color.brand.primary};
-              }
-            `}</style>
-          </Article>
-        )}
-      </ThemeContext.Consumer>
+  render() {
+    const {
+      data: {
+        posts: {
+          edges: posts,
+          technologies: technologies
+        }
+      }
+    } = this.props;
 
-      <Seo />
-    </React.Fragment>
-  );
+    // Create category list
+    const categories = {};
+    posts.forEach(edge => {
+      const {
+        node: {
+          frontmatter: { category }
+        }
+      } = edge;
+
+      if (category && category != null) {
+        if (!categories[category]) {
+          categories[category] = [];
+        }
+        categories[category].push(edge);
+      }
+    });
+
+    const categoryList = [];
+
+    for (var key in categories) {
+      categoryList.push([key, categories[key]]);
+    }
+
+    return (
+      <React.Fragment>
+        <ThemeContext.Consumer>
+          {theme => (
+            <Article theme={theme}>
+              <header>
+                <Headline title="Projects by category" theme={theme} />
+              </header>
+              <TechnologySelector technologies={technologies} onChange={change => console.log(change)} />
+              {categoryList.map(item => (
+                <section key={item[0]}>
+                  <h2>
+                    <FaTag /> {item[0]}
+                  </h2>
+                  <List edges={item[1]} theme={theme} />
+                </section>
+              ))}
+              {/* --- STYLES --- */}
+              <style jsx>{`
+                h2 {
+                  margin: 0 0 0.5em;
+                }
+                h2 :global(svg) {
+                  height: 0.8em;
+                  fill: ${theme.color.brand.primary};
+                }
+              `}</style>
+            </Article>
+          )}
+        </ThemeContext.Consumer>
+
+        <Seo />
+      </React.Fragment>
+    );
+  }
 };
 
 ProjectPage.propTypes = {
