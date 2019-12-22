5d69de0854e484f8c3fbad2850c004bba4774fce
Lucas Kjaero <lucas@lucaskjaero.com>
2019-12-22 13:49:55 -0800

    Oversharing

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,20 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import "prismjs/themes/prism-okaidia.css";
 
-import asyncComponent from "../AsyncComponent";
 import Headline from "../Article/Headline";
 import Bodytext from "../Article/Bodytext";
 import Meta from "./Meta";
 import Author from "./Author";
 import NextPrev from "./NextPrev";
-
-const Share = asyncComponent(() =>
-  import("./Share")
-    .then(module => {
-      return module.default;
-    })
-    .catch(error => {})
-);
 
 const Post = props => {
   const {
@@ -39,7 +30,6 @@ const Post = props => {
       </header>
       <Bodytext html={html} theme={theme} />
       <footer>
-        <Share post={post} theme={theme} />
         <Author note={authornote} theme={theme} />
         <NextPrev next={nextPost} prev={prevPost} theme={theme} />
       </footer>
