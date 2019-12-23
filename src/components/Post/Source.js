4577e3d0dc43c0b29a5437f432e4d734db1b342b
Lucas Kjaero <lucas@lucaskjaero.com>
2019-12-22 20:56:11 -0800

    View sources if available.

---
+++ src/components/Post/Source.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "antd/lib/button";
+
+const Source = props => {
+  const { source, theme } = props;
+  console.log(source);
+
+  if (source && source != "") {
+    return (
+      <React.Fragment>
+        <div className="source">
+          <Button type="primary" href={source}>View Source</Button>
+        </div>
+
+        {/* --- STYLES --- */}
+        <style jsx>{`
+          .ant-btn-primary {
+            background-color: ${theme.background.color.brand};
+          }
+        `}</style>
+      </React.Fragment>
+    );
+  } else {
+    return <br/ >
+  }
+};
+
+Source.propTypes = {
+  source: PropTypes.string,
+  theme: PropTypes.object.isRequired
+};
+
+export default Source;
--- src/templates/PostTemplate.js
+++ src/templates/PostTemplate.js
@@ -58,6 +58,7 @@ export const postQuery = graphql`
       frontmatter {
         title
         category,
+        source,
         technologies
       }
     }
