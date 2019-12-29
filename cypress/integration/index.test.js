020af92a0283bed474151ece82120d5509b85eb7
Lucas Kjaero <lucas@lucaskjaero.com>
2019-12-29 01:53:13 -0800

    Test index page

--- cypress/integration/index.test.js
+++ cypress/integration/index.test.js
@@ -2,7 +2,12 @@ describe("Check the home page", function() {
   beforeEach(function() {
     cy.visit("/");
   });
+
   it("Has my tagline", function() {
     cy.get("h2").should("contain", "Technologist. Traveler.");
+  });
+
+  it("Has my pitch", function() {
+    cy.get(".pitch").should("contain", "the");
   });
 });
