c09d7fa570464dfbe50c95ce94571a4b90882ca1
Lucas Kjaero <lucas@lucaskjaero.com>
2019-12-29 03:00:12 -0800

    Test templates

---
+++ cypress/integration/category.test.js
@@ -0,0 +1,14 @@
+describe("Check a category page", function() {
+  beforeEach(function() {
+    cy.visit("/category/full-stack");
+  });
+
+  it("Loads the category page", function() {
+    cy.get("span").should("contain", "Projects in category");
+    cy.get("h1").should("contain", "Full Stack");
+  });
+
+  it("Category should contain this website", function() {
+    cy.get("a").should("contain", "Lucas Kjaero-Zhang Website");
+  });
+});
