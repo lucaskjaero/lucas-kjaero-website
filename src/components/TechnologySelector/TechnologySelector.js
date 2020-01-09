07dd6ad359c7f422f87805d7be13a166fc9d71a7
Lucas Kjaero <lucas@lucaskjaero.com>
2020-01-08 21:14:41 -0800

    Switch selector component

--- src/components/TechnologySelector/TechnologySelector.js
+++ src/components/TechnologySelector/TechnologySelector.js
@@ -1,11 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Checkbox from "antd/lib/checkbox";
-import Col from "antd/lib/col";
-import Row from "antd/lib/row";
-import "antd/lib/checkbox/style/index.css";
-import "antd/lib/col/style/css";
-import "antd/lib/row/style/css";
+import DropdownTreeSelect from "react-dropdown-tree-select";
+import "react-dropdown-tree-select/dist/styles.css";
 import { TechnologyTree, TechnologiesInTree } from "./NestedTechnologies";
 
 const TechnologySelector = props => {
@@ -23,18 +19,8 @@ const TechnologySelector = props => {
   return (
     <React.Fragment>
       <div className="techselector">
-        <h2>Technologies</h2>
-        <Checkbox.Group style={{ width: "100%" }} onChange={onChange} defaultValue={technologies}>
-          <Row>
-            {technologies.map(tech => {
-              return (
-                <Col span={8} key={tech}>
-                  <Checkbox value={tech}>{tech}</Checkbox>
-                </Col>
-              );
-            })}
-          </Row>
-        </Checkbox.Group>
+        <h2>Filter by technologies used</h2>
+        <DropdownTreeSelect data={menu} onChange={onChange} />,
       </div>
 
       {/* --- STYLES --- */}
