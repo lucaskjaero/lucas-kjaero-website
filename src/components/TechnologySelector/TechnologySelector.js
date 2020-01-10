cb41c689c3e017c013d846a10a8d873d8f1b0d85
Lucas Kjaero <lucas@lucaskjaero.com>
2020-01-09 23:57:19 -0800

    Revert using the tree selector

--- src/components/TechnologySelector/TechnologySelector.js
+++ src/components/TechnologySelector/TechnologySelector.js
@@ -1,26 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
-import DropdownTreeSelect from "react-dropdown-tree-select";
-import "react-dropdown-tree-select/dist/styles.css";
+import Checkbox from "antd/lib/checkbox";
+import Col from "antd/lib/col";
+import Row from "antd/lib/row";
+import "antd/lib/checkbox/style/index.css";
+import "antd/lib/col/style/css";
+import "antd/lib/row/style/css";
 import { TechnologyTree, TechnologiesInTree } from "./NestedTechnologies";
 
 const TechnologySelector = props => {
   const { technologies: technologies, onChange: onChange, theme: theme } = props;
 
   // Add any technologies that don't have a specific hierarchy
-  const menu = technologies
-    .filter(x => !TechnologiesInTree.includes(x))
-    .map(item => {
-      return { label: item, value: item };
-    })
-    .concat(TechnologyTree)
-    .sort((a, b) => (a.label > b.label ? 1 : -1));
+  const unmatchedTechnologies = technologies
+    .filter(x => !TechnologiesInTree.has(x))
+    .sort((a, b) => (a > b ? 1 : -1));
 
   return (
     <React.Fragment>
       <div className="techselector">
         <h2>Filter by technologies used</h2>
-        <DropdownTreeSelect data={menu} onChange={onChange} />,
+        <Checkbox.Group style={{ width: "100%" }} onChange={onChange} defaultValue={technologies}>
+          <Row>
+            {technologies.map(tech => {
+              return (
+                <Col span={8} key={tech}>
+                  <Checkbox value={tech}>{tech}</Checkbox>
+                </Col>
+              );
+            })}
+          </Row>
+        </Checkbox.Group>
       </div>
 
       {/* --- STYLES --- */}
