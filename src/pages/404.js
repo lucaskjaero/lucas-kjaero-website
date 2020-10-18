d0fb847564cb99513fdac2af8b6790e81478da42
Lucas Kjaero <lucas@lucaskjaero.com>
2020-10-18 10:29:43 -0700

    404 page is visible

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Link } from "gatsby";
+import { ThemeContext } from "../layouts";
+import Article from "../components/Article";
+import Headline from "../components/Article/Headline";
+import Seo from "../components/Seo";
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <React.Fragment>
+      <ThemeContext.Consumer>
+        {theme => (
+          <Article theme={theme}>
+            <header>
+              <Headline title="Not Found" theme={theme} />
+            </header>
+            <div>
+              <p>You just hit a page that doesn't exist. Were you looking for <b><Link to="/project/">projects</Link></b> or <b><a href="/lucas-kjaero-zhang-resume.pdf">my resume</a></b>?</p>
+            </div>
+          </Article>
+        )}
+      </ThemeContext.Consumer>
+
+      <Seo />
+    </React.Fragment>
 );
 
 export default NotFoundPage;
